05f81d1af05bcf9055adf89af4234b7fa5db8cda
acai <15711058241@163.com>
2018-11-02 14:13:36 +0800

    The first star in the night sky

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2018-11-02 14:03:18
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2018-11-02 14:04:57
+ */
+header("content-type: text/html; charset=utf-8");
+echo "落霞与孤鹜齐飞，秋水共长天一色";
+?>
